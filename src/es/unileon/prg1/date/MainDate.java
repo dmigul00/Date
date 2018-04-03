2617beb7c21412d37a53ce50e6d90225a2b3df16
dmigul00 <dmigul00@estudiantes.unileon.es>
2018-04-03 10:59:49 -0700

    actualizado

--- src/es/unileon/prg1/date/MainDate.java
+++ src/es/unileon/prg1/date/MainDate.java
@@ -7,6 +7,7 @@ public class MainDate {
 		today = new Date(20, 3, 2018);
 		tomorrow = new Date(21, 3, 2018);
 		System.out.println(today + " isSameYear " + tomorrow + "? " + today.isSameYear(tomorrow));
+		
 	}
 
 }
